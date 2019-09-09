4820f4b25d886085a3ba9d8cbd203372f83fe2a8
Urfan Guliyev <urfan196@yahoo.com>
2019-09-09 20:14:57 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+pigeon_list = {}
+  data.each do | color_gender_lives, value|
+    values.each do |category, array|
+      array.each do |name|
+        pigeon_list[name] || = {}
+        pigeon_list [name][color_gender_lives] || = []
+        pigeon_list[name][color_gender_lives].push (category.to_s)
+      end
+    end
+ end
+  return pigeon_list
 end
