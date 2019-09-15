ffb73b51fe9e70149471302bbe35ed52cda3bc87
Emiley Palmquist <emiley.palmquist@icloud.com>
2019-09-15 00:01:39 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !new_hash.include?(name)
+          new_hash[name] = {}
+        end
+        
+        if !new_hash[name].include?(property)
+          new_hash[name][property] = []
+        end
+        
+        if !new_hash[name][property].include?(attribute)
+          new_hash[name][property].push(attribute.to_s)
+        end
+      end
+    end
+  end
+  new_hash
 end
